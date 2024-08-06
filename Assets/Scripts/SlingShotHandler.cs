dfd1f7836f872c7bd3fb41efec802b0729176668
darrench3140 <darrench3140@gmail.com>
2024-08-06 21:47:58 +0800

    init

---
+++ Assets/Scripts/SlingShotHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShotHandler : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+
+}
